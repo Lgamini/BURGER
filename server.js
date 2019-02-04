9365fc6b4069ebd01530f36e0181e488910371c1
Liliani <41878785+Lgamini@users.noreply.github.com>
2019-02-03 20:26:26 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+
+//Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var handlebars = require('express-handlebars');
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static("public"));
+//app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(bodyParser.json());
+
+//handlebars
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine','handlebars');
+
+//Import routes and give server access to them
+var routes = require('./controllers/burgers_controller');
+app.use(routes);
+
+app.listen(PORT, ()=>{
+    //Log (server-side) that server has started
+    console.log(`Connected on port: ${PORT}`);
+});
